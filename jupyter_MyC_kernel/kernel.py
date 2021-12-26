46c959c19f5c1955bfb1d46a8e3a1934440bb6a7
nufeng1999 <18478162@21cn.com>
2021-12-27 07:15:56 +0800

    update compile parameters

--- jupyter_MyC_kernel/kernel.py
+++ jupyter_MyC_kernel/kernel.py
@@ -1336,13 +1336,12 @@ class CKernel(MyKernel):
                 self.get_magicsSvalue(magics,'ldflags'),
                 self.get_magicsbykey(magics,'env'),
                 magics=magics)
-            while p.poll() is None:
-                p.write_contents()
+            returncode=p.wait_end(magics)
             p.write_contents()
             binary_file.name=os.path.join(os.path.abspath(''),outfile)
-            if p.returncode != 0:  # Compilation failed
-                self._log(''.join((str(s) for s in gcccmd))+"\n",3)
-                self._log("C compiler exited with code {}, the executable will not be executed".format(p.returncode),3)
+            if returncode != 0:  # Compilation failed
+                self._logln(''.join((str(s) for s in gcccmd))+"\n",3)
+                self._logln("C compiler exited with code {}, the executable will not be executed".format(returncode),3)
                 # delete source files before exit
                 os.remove(source_filename)
                 os.remove(binary_file.name)
